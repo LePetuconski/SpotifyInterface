27db6af989c8efe2fa46def94c5c4fd9ca8b2866
leticia <leticiapetuconski@gmai.com>
2023-10-12 23:35:31 -0300

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <body className="bg-zinc-900 text-zinc-50">{children}</body>
     </html>
   )
 }
